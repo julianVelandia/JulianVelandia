1044da91639285e01d45de823e02f3705e03962d
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2021-10-10 22:10:26 -0500

    articles

---
+++ nav.js
@@ -0,0 +1,5 @@
+var SearchBox = document.getElementById("searchBox");
+var SearchIcon = document.getElementById("searchIcon");
+SearchIcon.onclick = function(){
+    SearchBox.classList.toggle("active")
+} 
