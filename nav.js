2c11417abea5f2e6b8ba82ab58f86da6bb68d758
Julian Camilo Velandia <jvelandiag@unal.edu.co>
2022-02-14 15:43:20 -0500

    pr (#8)
    * changes (#7)
    
    * Changes
    
    * V.0.1.0 (#3)
    
    * first estructure (#1)
    
    * Responsive web (#2)
    
    * Responsive home
    
    * Footer
    
    * Article responsive
    
    * Redesing (#5) (#6)
    
    * boton inicio
    
    * nav and hero
    
    * new responsive button
    
    * icons
    
    * more articles
    
    * cv
    
    * CV
    
    * article github
    
    * code
    
    * first version done
    
    * fix bug
    
    * article copilot
    
    * Create CNAME
    
    * .
    
    * ddd

--- nav.js
+++ nav.js
@@ -3,3 +3,18 @@ var SearchIcon = document.getElementById("searchIcon");
 SearchIcon.onclick = function(){
     SearchBox.classList.toggle("navegation-active")
 } 
+
+var isDark = true
+var NavegationDark = document.getElementById("navegationDark");
+var NavegationDarkLuna = document.getElementById("navegationDarkLuna");
+NavegationDarkLuna.onclick = function(){
+    NavegationDark.classList.toggle("navegationLuz-active")
+    
+    if (isDark){
+        NavegationDarkLuna.src = "static/img/sol.png";
+        isDark = false
+    }else{
+        NavegationDarkLuna.src = "static/img/luna.png";
+        isDark = true
+    }
+} 
